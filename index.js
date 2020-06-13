f8cc5ab3a6a2754acc67167c2e97a2e4487b7796
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-06-13 20:47:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const path = require('path');
+const execFile = require('child_process').execFile;
+
+let args = process.argv;
+args.splice( 0, 2 );
+
+const exePath = path.join(__dirname, 'basisu.exe');
+console.log( exePath )
+
+
+console.log( chalk.green( 'basisu executing with arguments ' + args.join(' ') ) )
+
+execFile( exePath, args, { cwd: process.cwd() }, function( err, data ) {
+
+	console.log( err )
+
+	console.log( data.toString() );
+
+});
